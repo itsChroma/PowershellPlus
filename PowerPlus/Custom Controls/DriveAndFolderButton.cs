bdf2124f3cf8b01c3f8dadfc8c1ee927ac867229
itsChroma <Kbicknell14@hotmail.com>
2022-11-10 12:46:42 +0000

    Add more to UI

---
+++ PowerPlus/Custom Controls/DriveAndFolderButton.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+using System.Windows.Media;
+
+namespace PowerPlus.Custom_Controls
+{
+    public class DriveAndFolderButton : RadioButton
+    {
+        public PathGeometry Icon
+        {
+            get { return (PathGeometry)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register("Icon", typeof(PathGeometry), typeof(DriveAndFolderButton));
+    }
+}
